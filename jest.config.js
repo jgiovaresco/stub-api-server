04fa6dccdb42b73e179c2d3120a001b880424a99
Julien Giovaresco <dev@giovaresco.fr>
2020-05-24 14:33:09 +0200

    chore: configure test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  collectCoverageFrom: [
+    'src/**/*.{js,ts}',
+    '!src/**/__tests__/**',
+    '!src/**/*.d.ts',
+    '!test/**/*.ts',
+  ],
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'],
