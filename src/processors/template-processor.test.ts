41f13d3b00ea7f0c01768b151ecc230e562d0078
Julien Giovaresco <dev@giovaresco.fr>
2020-06-12 17:03:55 +0200

    refactor: remove ResponseGenerator class

--- src/processors/template-processor.test.ts
+++ src/processors/template-processor.test.ts
@@ -1,47 +1,21 @@
 import Bluebird from 'bluebird';
 
-import { ResponseGenerator } from './response-generator';
-import {
-  RequestPayload,
-  RequestQuery,
-  RouteConfig,
-  RequestContext,
-  Template,
-} from './route-config';
-import { newRouteConfig } from '../test/fixtures';
+import { RequestPayload, RequestQuery, RequestContext } from '../route-config';
+import { processTemplate } from './template-processor';
 
-describe('RouteGenerator should', () => {
-  function aGeneratorFor(config: RouteConfig) {
-    return new ResponseGenerator(config);
-  }
-
-  async function generatedBody(
-    template: Template,
-    context: RequestContext<unknown>,
-  ) {
-    const generator = aGeneratorFor(newRouteConfig({ template }));
-    const response = await generator.generate(context);
-    return response.body;
-  }
-
+describe('processTemplate should', () => {
   function newContext(query?: RequestQuery, payload?: RequestPayload<unknown>) {
     return { query, payload, url: 'http://localhost:8000' };
   }
 
-  it('generate a 200 response by default', async () => {
-    const route = newRouteConfig({ template: 'Hello World' });
+  it('process a simple value', async () => {
     const context = newContext();
-
-    const response = await aGeneratorFor(route).generate(context);
-    expect(response.status).toBe(200);
-  });
-
-  it('process a simple value template', async () => {
-    const context = newContext();
-    expect(await generatedBody(true, context)).toEqual(true);
-    expect(await generatedBody('Hello World', context)).toEqual('Hello World');
-    expect(await generatedBody(123, context)).toEqual(123);
-    expect(await generatedBody(null, context)).toEqual(null);
+    expect(await processTemplate(true, context)).toEqual(true);
+    expect(await processTemplate('Hello World', context)).toEqual(
+      'Hello World',
+    );
+    expect(await processTemplate(123, context)).toEqual(123);
+    expect(await processTemplate(null, context)).toEqual(null);
   });
 
   describe('process object templates', () => {
@@ -50,10 +24,10 @@ describe('RouteGenerator should', () => {
       const template = { message: 'Hello World' };
       const templateDeep = { root: { msg: 'Hello' } };
 
-      expect(await generatedBody(template, context)).toEqual({
+      expect(await processTemplate(template, context)).toEqual({
         message: 'Hello World',
       });
-      expect(await generatedBody(templateDeep, context)).toEqual({
+      expect(await processTemplate(templateDeep, context)).toEqual({
         root: { msg: 'Hello' },
       });
     });
@@ -64,7 +38,7 @@ describe('RouteGenerator should', () => {
       };
       const context = newContext({ name: 'John' });
 
-      expect(await generatedBody(template, context)).toEqual({
+      expect(await processTemplate(template, context)).toEqual({
         message: 'Hello John',
       });
     });
@@ -76,7 +50,7 @@ describe('RouteGenerator should', () => {
       };
       const context = newContext({}, { name: 'John' });
 
-      expect(await generatedBody(template, context)).toEqual({
+      expect(await processTemplate(template, context)).toEqual({
         message: 'Hello John',
       });
     });
@@ -88,7 +62,7 @@ describe('RouteGenerator should', () => {
         `Hello ${ctx.query?.name}`;
       const context = newContext({ name: 'John' });
 
-      expect(await generatedBody(template, context)).toEqual('Hello John');
+      expect(await processTemplate(template, context)).toEqual('Hello John');
     });
 
     it('using request body provided', async () => {
@@ -97,14 +71,14 @@ describe('RouteGenerator should', () => {
         `Hello ${ctx.payload?.name}`;
       const context = newContext({}, { name: 'John' });
 
-      expect(await generatedBody(template, context)).toEqual('Hello John');
+      expect(await processTemplate(template, context)).toEqual('Hello John');
     });
 
     it('using async function', async () => {
       const template = () => Bluebird.delay(10).then(() => 'Hello World');
       const context = newContext();
 
-      expect(await generatedBody(template, context)).toEqual('Hello World');
+      expect(await processTemplate(template, context)).toEqual('Hello World');
     });
   });
 });
